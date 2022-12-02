05d7eee0a3366a5ad7b36904acf262fc6aed7817
Dino Radoncic <94943001+ooDino@users.noreply.github.com>
2022-12-02 18:53:09 -0500

    Add files via upload

---
+++ tweakmart/static/js/entry.js
@@ -0,0 +1,15 @@
+var x = document.getElementById("login");
+var y = document.getElementById("register");
+var z = document.getElementById("btn");
+
+function registertab(){
+    x.style.left = "-400px";
+    y.style.left = "50px";
+    z.style.left = "110px";
+}
+
+function logintab(){
+    x.style.left = "50px";
+    y.style.left = "450px";
+    z.style.left = "0px";
+}
