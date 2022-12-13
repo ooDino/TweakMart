9d10343832158fb0358fab16118d5bbd4e307e3b
Konrad Zielinski <konradzielinski214@gmail.com>
2022-12-13 11:30:21 -0500

    remapping links for profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path("member/home", TemplateView.as_view(template_name="OU/memberhome.html"), name="memberhome"),
     path("login/", LoginView.as_view(authentication_form = CustomLoginForm), name="login"),
     path("profile/", TemplateView.as_view(template_name="OU/memberprofile.html"), name="memberprofile"),
+    path("profile/payment&wallet", TemplateView.as_view(template_name="OU/memberpaymentandwallet.html"), name="memberpaymentandwallet"),
+    path("profile/transaction", TemplateView.as_view(template_name="OU/membertransaction.html"), name="membertransaction"),
     path("profileupdate/", EditUserView, name="profileupdate",),
     path("profilepasswordupdate/", EditPassword, name="profilepasswordupdate",),
     # path("privacy/", auth_views.PasswordChangeView(template_name="OU/memberprivacy.html", success_url=reverse_lazy("memberprivacy")), name="password_change"),
